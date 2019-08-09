cd787babd297cf1c3da738afbee9d2733dfba849
techcto <smoore@solodev.com>
2019-08-09 12:40:15 -0400

    fix clean script

--- bin/cleanStacks.sh
+++ bin/cleanStacks.sh
@@ -3,7 +3,7 @@
 #echo $(aws cloudformation describe-stacks)
 for BUCKET in $(aws s3api list-buckets --region us-east-1 | ./jq -r '.Buckets[].Name')
 do
-    if [[ $BUCKET == *"eks-tmp-"* ]]; then
+    if [[ $BUCKET == "eks-tmp-"* ]]; then
         echo "I found one. Time to delete a bucket: ${BUCKET}.  Bye-Bye!"
         aws s3 rb s3://$BUCKET --force 
     fi
@@ -11,7 +11,7 @@ done
 
 for STACK in $(aws cloudformation describe-stacks --region us-east-1 | ./jq -r '.Stacks[].StackName')
 do
-    if [[ $STACK == *"eks-tmp-"* ]]; then
+    if [[ $STACK == "eks-tmp-"* ]]; then
         echo "I found one. Time to delete stack: ${STACK}.  Bye-Bye!"
         aws cloudformation delete-stack --stack-name $STACK
     fi
