75260f734db453ef6ff813f633ec9f31c25dacdf
techcto <smoore@solodev.com>
2019-09-16 10:58:23 -0400

    update deployment

--- bin/stacks.sh
+++ bin/stacks.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 
-echo "Create Solodev DCX for EKS"
-echo $(aws s3 cp s3://build-secure/params/solodev-dcx-eks.json - ) > solodev-dcx-eks.json
+CLUSTER=0
+DEPLOYMENT=1
+
+if [ $CLUSTER == 1 ]; then
+
+echo "Create AWS EKS Cluster"
+echo $(aws s3 cp s3://build-secure/params/amazon-eks.json - ) > amazon-eks.json
 aws cloudformation create-stack --disable-rollback --stack-name eks-tmp-${DATE} --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
-    --parameters file:///${CODEBUILD_SRC_DIR}/solodev-dcx-eks.json \
+    --parameters file:///${CODEBUILD_SRC_DIR}/amazon-eks.json \
+    --template-url https://s3.amazonaws.com/solodev-quickstarts/eks/amazon-eks.yaml \
+    # --notification-arns $NOTIFICATION_ARN
+
+fi
+
+if [ $DEPLOYMENT == 1 ]; then
+
+echo "Install Solodev DCX"
+echo $(aws s3 cp s3://build-secure/params/solodev-dcx.json - ) > solodev-dcx.json
+aws cloudformation create-stack --disable-rollback --stack-name solo-tmp-${DATE} --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+    --parameters file:///${CODEBUILD_SRC_DIR}/solodev-dcx.json \
     --template-url https://s3.amazonaws.com/solodev-quickstarts/eks/solodev-dcx.yaml \
     # --notification-arns $NOTIFICATION_ARN
+
+fi
