760a5b4ee70f7f8974371a8b0b397ad8365b9e47
techcto <smoore@solodev.com>
2019-10-22 09:42:53 -0400

    ..

--- eks/scripts/bastion_bootstrap.sh
+++ eks/scripts/bastion_bootstrap.sh
@@ -584,6 +584,7 @@ initWeave(){
 }
 
 initServiceAccount(){
+    kubectl --kubeconfig=$KUBECONFIG create namespace solodev-dcx
     echo "aws eks describe-cluster --name ${K8S_CLUSTER_NAME} --region ${REGION} --query cluster.identity.oidc.issuer --output text"
     ISSUER_URL=$(aws eks describe-cluster --name ${K8S_CLUSTER_NAME} --region ${REGION} --query cluster.identity.oidc.issuer --output text )
     echo $ISSUER_URL
