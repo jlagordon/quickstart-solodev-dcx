d7b2938f45783f7015f72dea9c6ab185a2eb2988
techcto <smoore@solodev.com>
2019-11-07 16:09:19 -0500

    ..

--- eks/scripts/bastion_bootstrap.sh
+++ eks/scripts/bastion_bootstrap.sh
@@ -674,7 +674,7 @@ initNetwork(){
 
 initDashboard(){
     yum install -y jq
-    DOWNLOAD_URL=$(curl --silent "https://api.github.com/repos/kubernetes-incubator/metrics-server/releases/latest" | jq -r .tarball_url)
+    DOWNLOAD_URL=$(curl --silent "https://api.github.com/repos/kubernetes-sigs/metrics-server/releases/latest" | jq -r .tarball_url)
     DOWNLOAD_VERSION=$(grep -o '[^/v]*$' <<< $DOWNLOAD_URL)
     curl -Ls $DOWNLOAD_URL -o metrics-server-$DOWNLOAD_VERSION.tar.gz
     mkdir metrics-server-$DOWNLOAD_VERSION
